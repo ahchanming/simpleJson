903a10711a161951e3dffe40cd49651f53d53614
ahchanming@gmail.com <nemo11092>
2016-03-27 10:36:07 +0800

    first commit

---
+++ test/src/java/SimpleJsonTest.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Administrator on 2016/3/27.
+ */
+public class SimpleJsonTest {
+    public static void main(String[] args){
+        System.out.println("HelloWorld!!!!");
+    }
+}
